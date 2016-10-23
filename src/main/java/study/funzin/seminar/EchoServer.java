4326cd1770e263bf101a04b9e17a652ed6f4a560
jaejin <starcx@naver.com>
2016-10-23 12:56:53 +0900

    주말 공부
    집에서 6장까지 해야함

--- src/main/java/study/funzin/seminar/EchoServer.java
+++ src/main/java/study/funzin/seminar/EchoServer.java
@@ -1,15 +1,15 @@
 package study.funzin.seminar;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelPipeline;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.oio.OioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.oio.OioServerSocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 
 /**
  * Created by JE on 2016-10-20.
@@ -42,21 +42,43 @@ public class EchoServer {
                     // 블로킹 모드
 //                    .channel(OioServerSocketChannel.class)
 
+                    // 채널에서 발생하는 모든 이벤트를 로그로 출력해준다.
+                    // 인수는 출력할 로그의 레벨을 지정한다.
+                    .handler(new LoggingHandler(LogLevel.INFO))
+
                     // 자식 채널의 초기화 방법을 설정한다. 여기서는 익명 클래스로 채널 초기화 방법을 지정했다.
+                    // childHandler 메서드를 통해서 연결된 클라이언트 소켓 채널이 사용할 채널 파이프라인을 설정한다.
                     .childHandler(new ChannelInitializer<SocketChannel>() {
 
                         @Override
                         // ChannelInitializer 는 클라이언트로부터 연결된 채널이 초기화될 떄의 기본 동작이 지정된 추상 클래스다.
+                        // 클라이언트 소켓 채널이 생성될 때 자동으로 호출되는데 이때 채널 파이프라인의 설정을 수행한다.
                         public void initChannel(SocketChannel sc) {
                             // 채널 파이프라인 객체를 생성한다.
+                            // initChannel 메서드의 인자로 입력된 소켓 채널(즉, 연결된 클라이언트 소켓 채널)에 설정된 채널 파이프라인을 가져오게 되는데,
+                            // 네티의 내부에서는 클라이언트 소켓 채널을 생서할 때 빈 채널 파이프라인 객체를 생성하여 할당한다.
                             ChannelPipeline cp = sc.pipeline();
+                            // 클라이언트 소켓 채널의 파이프라인에 등록
+                            // 서버 소켓채널로 연결된 클라이언트 채널에 파이프라인을 설정하는 역할을 수행
+                            cp.addLast(new LoggingHandler(LogLevel.INFO));
+                            // 네티가 제공하는 HTTP 서버 코덱이다.
+                            // 인바운드와 아웃바운드 핸들러를 모두 구현한다.
+//                            cp.addLast(new HttpServerCodec());
                             // 접속된 클라이언트로부터 수신된 데이터를 처리할 핸들러 지정
                             // 채널 파이프라인에 EchoServerHandler 클래스를 등록한다.
                             // EchoServerHandler 클래스는 이후에 클라이언트의 연결이 생성되었을 때 데이터 처리를 담당한다.
+                            // 이벤트 핸들러인 EchoServerHandler 를 채널 파이프라인에 등록하려면 채널 파이프라인의 addLast 메서드를 사용한다.
                             cp.addLast(new EchoServerHandler());
+                            // 두 번째 핸들러
+//                            cp.addLast(new EchoServerHandler2());
                         }
 
                     })
+
+                    // 동시에 수용할 클라이언트의 연결 요청 수
+//                    .option(ChannelOption.SO_BACKLOG, 1)
+                    // 커널 버퍼에 남은 데이터를 상대방 소켓 채널로 모두 전송하고 즉시 연결을 끊는다.
+//                    .childOption(ChannelOption.SO_LINGER, 0)
             ;
 
             //부트스트랩 클래스의 bind 메서드로 접속할 포트 지정
--- src/main/java/study/funzin/seminar/EchoServerHandler2.java
+++ src/main/java/study/funzin/seminar/EchoServerHandler2.java
@@ -1,22 +1,15 @@
 package study.funzin.seminar;
 
-import com.sun.org.apache.xerces.internal.impl.io.UTF8Reader;
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.handler.codec.http.*;
 
 import java.nio.charset.Charset;
 
 /**
- * Created by JE on 2016-10-20.
+ * Created by jin on 16. 10. 23.
  */
-// 상속 받아서 재정의
-public class EchoServerHandler extends SimpleChannelInboundHandler<Object>{
-
-    private static final byte[] CONTENT = {'H', 'E', 'L', 'L', 'O'};
+public class EchoServerHandler2 extends SimpleChannelInboundHandler<Object> {
 
     @Override
     public void messageReceived(ChannelHandlerContext channelHandlerContext, Object msg){
@@ -27,20 +20,6 @@ public class EchoServerHandler extends SimpleChannelInboundHandler<Object>{
     // 데이터 수신 이벤트 처리 메서드, 클라이언트로부터 데이터의 수신이 이루어졌을 때 네티가 자동으로 호출하는 이벤트 메서드
     public void channelRead(ChannelHandlerContext ctx, Object msg){
         // Discard 서버에서 지정한 포트로 접속한 클라이언트가 데이터를 전송하면 자동으로 실행됨
-
-        //수신한 객체가 HttpRequest 일 때, HttpResponse 객체를 생성하고 헤더와 메세지를 저장한다.
-        if(msg instanceof HttpRequest){
-            HttpRequest req = (HttpRequest) msg;
-            System.out.println(req.getUri());
-            if(HttpHeaders.is100ContinueExpected(req)){
-                ctx.write(new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.CONTINUE));
-            }
-            boolean keepAlive = HttpHeaders.isKeepAlive(req);
-            FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, Unpooled.wrappedBuffer(CONTENT));
-            if(keepAlive){
-                ctx.write(response).addListener(ChannelFutureListener.CLOSE);
-            }
-        }
 
         // 수신된 데이터를 가지고 있는 네티의 바이트 버퍼 객체로부터 문자열 데이터를 읽어온다.
         // java jvm default charset
@@ -54,9 +33,6 @@ public class EchoServerHandler extends SimpleChannelInboundHandler<Object>{
         // ChannelHandlerContext는 인터페이스 객체로서 채널 파이프라인에 대한 이벤트를 처리한다.
         // 서버에 연결된 클라이언트 채널로 입력받은 데이터를 그래도 전송한다.
         ctx.write(msg);
-
-        // 채널에 파이프라인에 channelRead 이벤트를 발생시킨다.
-//        ctx.fireChannelRead(msg);
 
     }
 
