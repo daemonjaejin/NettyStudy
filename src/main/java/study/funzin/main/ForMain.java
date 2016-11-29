0f08b1786d29709e3a4edb687d84b69845d289a3
lee jaejin <starcx@naver.com>
2016-11-29 18:38:25 +0900

    For문 테스트

---
+++ src/main/java/study/funzin/main/ForMain.java
@@ -0,0 +1,17 @@
+package study.funzin.main;
+
+/**
+ * Created by JE on 2016-11-29.
+ */
+public class ForMain {
+
+    public static void main(String [] args){
+
+        int last = 5;
+        for (int i = last; i >  0; i--) {
+            System.out.println(i);
+        }
+
+    }
+
+}
