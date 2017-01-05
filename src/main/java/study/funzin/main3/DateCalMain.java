6b08579cf91ad2315dd85a1f16007c4fddc838c4
lee jaejin <starcx@naver.com>
2017-01-05 20:47:32 +0900

    Tail Test

---
+++ src/main/java/study/funzin/main3/DateCalMain.java
@@ -0,0 +1,34 @@
+package study.funzin.main3;
+
+import study.funzin.util.DateUtil;
+
+import java.util.Date;
+
+/**
+ * Created by JE on 2017-01-05.
+ */
+public class DateCalMain {
+
+    private static final String DATE_FORMAT = "yyyyMMdd-HH";
+
+    public static void main(String [] args){
+
+        StringBuilder sb = new StringBuilder();
+
+        System.out.println(sb.length());
+
+        for (int i = 0; i < 10; i++) {
+            if(i<5){
+                continue;
+            }
+            System.out.println("test"+(i+1));
+        }
+
+//        for (int i = 168; i > 0; i--) {
+//            String dateStr = DateUtil.formatDate(DateUtil.hourAdd(new Date(), (i * -1)), DATE_FORMAT);
+//            System.out.println("dateStr : " + dateStr);
+//        }
+
+    }
+
+}
