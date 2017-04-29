52c4cb12a8df18a90907ef87b2370c27f75972f1
jaejin <jaejin@queryjet.com>
2017-04-29 14:07:02 +0900

    20170429

---
+++ src/main/java/study/funzin/main5/IntegerTest.java
@@ -0,0 +1,13 @@
+package study.funzin.main5;
+
+/**
+ * Created by jin on 17. 4. 24.
+ */
+public class IntegerTest {
+
+    public static void main(String [] args){
+        int value = Integer.parseInt( "0100", 16 );
+        System.out.println("value : " + value);
+    }
+
+}
